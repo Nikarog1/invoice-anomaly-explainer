46e150fd51246b5c184ad22e86d4816207391c31
Nikarog1 <nikita.rogozhnikov@gmail.com>
2026-04-21 18:10:54 +0200

    Add initial sqlite.py file with database setup and session management

---
+++ data/sqlite.py
@@ -0,0 +1,18 @@
+from sqlmodel import Session, SQLModel, create_engine
+
+from config.settings import settings
+
+from schemas.anomaly import AnomalyFlag, AnomalyReport
+from schemas.contract import Contract, ContractLineItem
+from schemas.invoice import Invoice, InvoiceLineItem
+from schemas.junction import LineItemMatch
+
+
+
+engine = create_engine(settings.sqlite_url, echo=True) # in prod change to echo=False; potentially add async option connect_args={"check_same_thread": False}
+
+def create_db_and_tables():  
+    SQLModel.metadata.create_all(engine)
+    
+def get_session():
+    return Session(engine)
