3d1c0478863b2f83b575588586fb761b155784b5
Nikarog1 <nikita.rogozhnikov@gmail.com>
2026-04-23 16:43:44 +0200

    Add PipelineStateError to catch missing fields in state required for node to run

--- core/exceptions.py
+++ core/exceptions.py
@@ -9,11 +9,19 @@ class InvalidCSVError(Exception):
     
     def __init__(self, path: Path|str) -> None:
         self.path = path
-        super().__init__(f"File must have csv format: {path}")
+        super().__init__(f"File must have csv format: {path}.")
 
 class InvoiceNotFoundError(Exception):
     """Raised when the provided invoice_id is not found in database."""
 
     def __init__(self, invoice_id: UUID) -> None:
         self.invoice_id = invoice_id
-        super().__init__(f"Invoice with id {invoice_id} not found in database")
+        super().__init__(f"Invoice with id {invoice_id} not found in database.")
+        
+class PipelineStateError(Exception):
+    """Raised when pipeline state misses required input from previous node(s)."""
+    
+    def __init__(self, field_name: str):
+        self.field_name = field_name
+        super().__init__(f"Required state field '{field_name}' is None.")
+    
