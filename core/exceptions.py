437309f8d5a138faedb558f6e9fe0fc00f888dbf
Nikarog1 <nikita.rogozhnikov@gmail.com>
2026-04-23 18:06:29 +0200

    Refactor exception messages for clarity and consistency in exceptions.py

--- core/exceptions.py
+++ core/exceptions.py
@@ -4,7 +4,7 @@ from uuid import UUID
 
 
 class InvalidCSVError(Exception):
-    """Raised when a file fails CSV validation —
+    """Raised when file fails CSV validation —
     either by extension check or content sniffing."""
     
     def __init__(self, path: Path|str) -> None:
@@ -12,11 +12,18 @@ class InvalidCSVError(Exception):
         super().__init__(f"File must have csv format: {path}.")
 
 class InvoiceNotFoundError(Exception):
-    """Raised when the provided invoice_id is not found in database."""
+    """Raised when provided invoice_id is not found in database."""
 
     def __init__(self, invoice_id: UUID) -> None:
         self.invoice_id = invoice_id
         super().__init__(f"Invoice with id {invoice_id} not found in database.")
+        
+class InvoiceValueNotFoundError(Exception):
+    """Raised when provided invoice has field required for further analysis equal None."""
+
+    def __init__(self, field: str) -> None:
+        self.field = field
+        super().__init__(f"Invoice field {self.field} is None")
         
 class PipelineStateError(Exception):
     """Raised when pipeline state misses required input from previous node(s)."""
