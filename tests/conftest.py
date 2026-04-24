04771999b0383d4b03618399ec6a15ed4e8af1d8
Nikarog1 <nikita.rogozhnikov@gmail.com>
2026-04-24 12:31:09 +0200

    Add metadata cleanup in fake_session fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ import os
 os.environ["SQLITE_URL"] = "sqlite:///:memory:"
 
 import pytest
-from sqlmodel import Session
+from sqlmodel import Session, SQLModel
 from data.sqlite import engine, create_db_and_tables
 
 
@@ -12,3 +12,4 @@ def fake_session():
     create_db_and_tables()
     with Session(engine) as session:    
         yield session
+    SQLModel.metadata.drop_all(engine)
