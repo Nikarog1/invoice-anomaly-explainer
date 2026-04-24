7649f80ced2a3a50e85365482436aeefb7d0349c
Nikarog1 <nikita.rogozhnikov@gmail.com>
2026-04-24 15:43:45 +0200

    Add IngestionResult model to store normalization results with invoice and line items

--- ingestion/models.py
+++ ingestion/models.py
@@ -1,6 +1,16 @@
-from pydantic import BaseModel 
+from pydantic import BaseModel
+
+from schemas.invoice import Invoice, InvoiceLineItem
+from schemas.columns_mapping import ColumnMapping
 
 
 
 class RawInvoice(BaseModel):
     model_config = {"extra": "allow"}
+    
+
+class IngestionResult(BaseModel):
+    """In-memory class to store result from normalization / ingestion phase."""
+    invoice: Invoice
+    invoice_line_items: list[InvoiceLineItem]
+    column_mapping_results: list[ColumnMapping]
