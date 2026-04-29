5a36c128fd289592075e7e8ffd95f623903f4b1c
Nikarog1 <nikita.rogozhnikov@gmail.com>
2026-04-29 21:28:52 +0200

    Add embedding_model_name to Settings configuration

--- config/settings.py
+++ config/settings.py
@@ -26,6 +26,7 @@ class Settings(BaseSettings):
     columns_mapping_path: Path = Path("./config/columns_mapping.json")
     ollama_base_url: str = "http://localhost:11434"
     model_name: str = "mistral"
+    embedding_model_name: str = "nomic-embed-text"
     sqlite_url: str = "sqlite:///./data/data.db"
     chromadb_path: Path = Path("./data/chroma")
     fastapi_host: str = "127.0.0.1"
