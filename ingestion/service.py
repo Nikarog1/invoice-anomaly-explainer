ae960938630a3e66c6ac3d227873e4199b00670d
Nikarog1 <nikita.rogozhnikov@gmail.com>
2026-05-13 19:05:19 +0200

    Remove unused parameters from IngestionService.run method for cleaner code

--- ingestion/service.py
+++ ingestion/service.py
@@ -18,8 +18,6 @@ class IngestionService:
             path_to_csv: Path | str,
             path_to_mapping: Path | str = settings.columns_mapping_path,
             confidence_threshold: float = settings.invoice_ingestion.invoice_fuzzy_match_min,
-            ollama_url: str = settings.ollama_base_url,
-            model_name: str = settings.model_name,
     ) -> UUID:
         """
         Run whole ingestion pipeline to extract, normalize, and save for further processing.
@@ -28,8 +26,7 @@ class IngestionService:
             path_to_csv: path to invoice in csv format
             path_to_mapping: path to columns_mapping
             confidence_threshold: confidence threshold for fuzzy match mapping from config/settings.py
-            ollama_url: ollama url of local model
-            model_name: model name performing validation
+
         """
         logger.info(f"Ingestion started for {path_to_csv}")
         raw_fields = CSVParser(path=path_to_csv).parse()
@@ -38,8 +35,6 @@ class IngestionService:
             raw_fields,
             path=path_to_mapping,
             confidence_threshold=confidence_threshold,
-            ollama_url=ollama_url,
-            model_name=model_name,
         )
         ingestion_result = await normalizer.normalize()
         invoice_id = ingestion_result.invoice.invoice_id
