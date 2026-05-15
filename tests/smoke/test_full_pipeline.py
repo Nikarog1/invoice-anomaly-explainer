a2d364c26dcf0f9f0abc22cb5e86c677429a44c5
Nikarog1 <nikita.rogozhnikov@gmail.com>
2026-05-15 17:53:24 +0200

    Add smoke test for degraded path and enhance invoice generation validation

--- tests/smoke/test_full_pipeline.py
+++ tests/smoke/test_full_pipeline.py
@@ -95,3 +95,38 @@ async def test_full_pipeline(tmp_path: Path) -> None:
     assert report.anomalies_count >= 1
     assert report.agent_explanation
     assert report.explanation_date
+    
+
+@pytest.mark.smoke
+async def test_degraded_path(tmp_path: Path) -> None:
+    items_1 = [
+        ["012344", "suppl1", "2025-08-31", "our_company", "1000.0", "CZK", "table", "1.0", "416.66", "20.0", "500.0", "meta_field"],
+        ["012345", "suppl1", "2025-09-30", "our_company", "1000.0", "CZK", "table", "1.0", "416.66", "20.0", "500.0", "meta_field"],
+        ["012346", "suppl1", "2025-10-31", "our_company", "1000.0", "CZK", "table", "1.0", "416.66", "20.0", "500.0", "meta_field"],
+        ["012347", "suppl1", "2025-11-30", "our_company", "1000.0", "CZK", "table", "1.0", "416.66", "20.0", "500.0", "meta_field"],
+        ["012348", "suppl1", "2025-12-31", "our_company", "2000.0", "CZK", "table", "1.0", "833.33", "20.0", "1000.0", "meta_field"],
+    ]
+    items_2 = [
+        ["012344", "suppl1", "2025-08-31", "our_company", "1000.0", "CZK", "chair", "1.0", "416.66", "20.0", "500.0", "meta_field"],
+        ["012345", "suppl1", "2025-09-30", "our_company", "1000.0", "CZK", "chair", "1.0", "416.66", "20.0", "500.0", "meta_field"],
+        ["012346", "suppl1", "2025-10-31", "our_company", "1000.0", "CZK", "chair", "1.0", "416.66", "20.0", "500.0", "meta_field"],
+        ["012347", "suppl1", "2025-11-30", "our_company", "1000.0", "CZK", "chair", "1.0", "416.66", "20.0", "500.0", "meta_field"],
+        ["012348", "suppl1", "2025-12-31", "our_company", "2000.0", "CZK", "chair", "2.0", "416.66", "20.0", "1000.0", "meta_field"],
+    ]
+
+    invoice_id = None
+    ingestion = IngestionService()
+    
+    for i, (item1, item2) in enumerate(zip(items_1, items_2)):
+        path = _generate_invoices(tmp_path, item1, item2, i)
+        invoice_id = await ingestion.run(path_to_csv=path) 
+    assert invoice_id
+    assert isinstance(invoice_id, UUID)
+
+    report = await run_pipeline(invoice_id)
+    
+    assert report
+    assert report.invoice_id == invoice_id
+    assert report.anomalies_count >= 1
+    assert report.agent_explanation
+    assert report.explanation_date
